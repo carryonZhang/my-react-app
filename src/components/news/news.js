db241a16ec547a928d92b641d92942d458a0d401
carryonZhang <xunlie2008@126.com>
2017-06-17 15:22:51 +0800

    代码修改

--- src/components/news/news.js
+++ src/components/news/news.js
@@ -2,19 +2,20 @@
  * Created by shaolong on 2017/6/15.
  */
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './styles/newslist.css';
 
 const News = (props) => {
 	let title = props.ns.title ? props.ns.title : '';
 	let content = props.ns.content ? props.ns.content : '';
 	return (
-		<a className="news-wrap flex-left" href="#">
+		<Link to="/detail" className="news-wrap flex-left" href="#">
 			<div className="image unit-1-3"></div>
 			<div className="news unit-2-3">
 				<h2 className="title">{ title }</h2>
 				<div className="content">{ content }</div>
 			</div>
-		</a>
+		</Link>
 	)
 }
 export default News;
--- src/routers.js
+++ src/routers.js
@@ -2,19 +2,18 @@
  * Created by shaolong on 2017/6/13.
  */
 import React, { Component } from 'react';
-import { Router, Route } from 'react-router';
-import createBrowserHistory from 'history/createBrowserHistory'
+import { HashRouter, Route } from 'react-router-dom';
 
 import App from './container/app/App';
 
-const history = createBrowserHistory();
 
 class Routers extends Component {
+
 	render() {
 		return (
-			<Router history={history} key={Math.random()}>
+			<HashRouter key={Math.random()}>
 				<Route path="/" component={App}></Route>
-			</Router>
+			</HashRouter>
 		)
 	}
 }
